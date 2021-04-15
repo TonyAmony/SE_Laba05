e5b85eedf8f0deef7a3140f59ce7a08ed9c71bde
Anton Mokshantsev <tony.forfuture@gmail.com>
2021-04-15 16:03:53 +0300

    Added Multiplication

--- main.cpp
+++ main.cpp
@@ -1,13 +1,12 @@
-.//This is program for education
-#include <iostream>
+#include "iostream"
 
-using namespace std;
-
-int main()
-{
-    int a, b,c ;
-    cout << "Welcome";
-    cin >> a >> b;
-    c = a - b;
-    cout << c;
-}
+3 // This program is just an example one under VCS.
+4 int main()
+5 {
+6 int a, b;
+7 cout << "Enter A and B: ";
+8 cin >> a >> b;
+9 cout << "A + B = " << a + b << '\n'
+10 << "A - B = " << a - b << '\n'
+11 << "A * B = " << a * b << '\n';
+12 }
